0949dc75fdb1e86ee543411c93ca621b136a58ff
Mathemalik <Miftaugromalik@gmail.com>
2020-08-07 00:03:21 +0700

    tugas react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DaftarHargaBuah from './Tugas 14/Tugas14.js';
+
+function App() {
+  return (
+    <div>
+      <DaftarHargaBuah />
+      <Timer/>
+    </div>
+  );
+}
+
+export default App;
